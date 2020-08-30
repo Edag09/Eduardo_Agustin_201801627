227a0618d1bc73874604bc192760a445eeffdaad
Edag09 <3006840880101@ingenieria.usac.edu.gt>
2020-08-30 00:27:15 -0600

    Seguro

--- Lenguajes/Codigo/Metodos.py
+++ Lenguajes/Codigo/Metodos.py
@@ -1,13 +1,11 @@
-# Clase con métodos y atributos de una pila.
 class Metodos:
-    # Listas para definir si el valor a evaluar es un número, letra o operador.
-    letras = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    # Lista de numero
     numeros = list('0123456789')
 
     # Atributos de una pila
-    tamanio = 0  # Tamaño máximo de la pila
-    pila = []
-    tope = 0
+    tamanio = 0  # Tamaño minimo de la pila
+    stack = []
+    top = 0
 
     # Esta lista almacena la expresión convertida.
     expresion_postfija = []
@@ -16,35 +14,34 @@ class Metodos:
     def __init__(self, tamanio, expresion_postfija):
         self.tamanio = tamanio
         self.expresion_postfija = expresion_postfija
-        self.tope = -1
-        self.letras = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        self.top = -1
         self.numeros = list('0123456789')
 
     # Método para definir si la pila está llena
     def pila_llena(self):
-        if self.tope == self.tamanio:  # Si el tope tiene el mismo valor del tamaño entonces la pila está llena.
+        if self.top == self.tamanio:  # Si el tope tiene el mismo valor del tamaño entonces la pila está llena.
             print("La pila esta llena")
             return True
         return False
 
     # Método para definir si la pila esta vacía
     def pila_vacia(self):
-        if self.tope == -1:  # si el valor del tope es igual a -1 entonces la pila esta vacía.
+        if self.top == -1:  # si el valor del tope es igual a -1 entonces la pila esta vacía.
             return True
         return False
 
     # Método para apilar un valor en la pila
     def apilar(self, dato):
         if not self.pila_llena():  # Si la pila no está llena entonces añade el dato e incrementa el tope de la pila.
-            self.pila.insert(self.tope, dato)
-            self.tope += 1
+            self.stack.insert(self.top, dato)
+            self.top += 1
 
     # Método para desapilar (quitar) un valor en la pila.
     def desapilar(self):
         if not self.pila_vacia():
-            aux = self.pila[self.tope]
-            del self.pila[self.tope]
-            self.tope -= 1
+            aux = self.stack[self.top]
+            del self.stack[self.top]
+            self.top -= 1
             return aux
 
     # Método que define la prioridad los operadores en la expresión.
@@ -75,28 +72,28 @@ class Metodos:
 
     # Método que define la prioridad de los operadores en la pila.
     def prioridad_pila(self):
-        if self.pila[self.tope] == 'Pow':
+        if self.stack[self.top] == 'Pow':
             prioridadpila = 4
             return prioridadpila
-        elif self.pila[self.tope] == 'Sqrt':
+        elif self.stack[self.top] == 'Sqrt':
             prioridadpila = 4
             return prioridadpila
-        elif self.pila[self.tope] == '*':
+        elif self.stack[self.top] == '*':
             prioridadpila = 1
             return prioridadpila
-        elif self.pila[self.tope] == '/':
+        elif self.stack[self.top] == '/':
             prioridadpila = 1
             return prioridadpila
-        elif self.pila[self.tope] == '+':
+        elif self.stack[self.top] == '+':
             prioridadpila = 2
             return prioridadpila
-        elif self.pila[self.tope] == '-':
+        elif self.stack[self.top] == '-':
             prioridadpila = 2
             return prioridadpila
-        elif self.pila[self.tope] == '(':
+        elif self.stack[self.top] == '(':
             prioridadpila = 0
             return prioridadpila
-        elif self.pila[self.tope] == ')':
+        elif self.stack[self.top] == ')':
             prioridadpila = 0
             return prioridadpila
 
@@ -104,6 +101,6 @@ class Metodos:
 
     # Método que define si el dato es un operador, una letra o un numero
     def es_operador(self, ei, i):
-        if ei[i] in self.letras or ei[i] in self.numeros:
+        if ei[i] in self.numeros:
             return False
         return True
