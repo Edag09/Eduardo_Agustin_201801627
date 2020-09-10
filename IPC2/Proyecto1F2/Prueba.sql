d68ab44ba9d19ac153f25edae803052afa1bbe0b
Edag09 <3006840880101@ingenieria.usac.edu.gt>
2020-09-10 00:38:08 -0600

    tablas

--- IPC2/Proyecto1F2/Prueba.sql
+++ IPC2/Proyecto1F2/Prueba.sql
@@ -1,6 +1,6 @@
 create database Proyecto1F2
 
-CREATE TABLE Usuario (
+CREATE TABLE Usuario(
 Id INT PRIMARY  KEY IDENTITY NOT NULL,
 Nombre VARCHAR(40) NOT NULL,
 Apellido VARCHAR(40) NOT NULL,
@@ -13,6 +13,17 @@ Correo VARCHAR(70)
 
 CREATE TABLE Partida(
 Id_Partida INT PRIMARY KEY IDENTITY NOT NULL,
-Estado_Jugador VARCHAR(50) NOT NULL,
+Estado_Jugador varchar(50) not null,
+No_Movimiento int not null
+);
 
+CREATE TABLE Tipo_Partida(
+Id_TPartida INT PRIMARY KEY IDENTITY NOT NULL,
+Tipo_Partida varchar(50) not null,
+No_Partida int not null
+);
+
+CREATE TABLE Torneo(
+Id_Torneo INT PRIMARY KEY IDENTITY NOT NULL,
+Estado_Jugador Varchar(60) not null
 );
