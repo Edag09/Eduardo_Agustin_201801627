71e7e5aa5b3c13649647595623e595634995907d
Edag09 <3006840880101@ingenieria.usac.edu.gt>
2020-09-20 00:43:55 -0600

    Correcciones Documentacion y BD

--- IPC2/Proyecto1F2/BaseDeDatos.sql
+++ IPC2/Proyecto1F2/BaseDeDatos.sql
@@ -1,5 +1,6 @@
 create database P1F2
 
+
 use P1F2
 
 CREATE TABLE Usuario(
@@ -14,14 +15,14 @@ Correo varchar(70)
 
 CREATE TABLE Partida(
 Id_Partida int primary key identity NOT NULL,
-Estado_Jugador varchar(50) not null,
-No_Movimiento int not null
+Fecha Date
 );
 
 CREATE TABLE Tipo_Partida(
 Id_TPartida int primary key identity NOT NULL,
 Tipo_Partida varchar(50) not null,
-No_Partida int not null,
+Estado_Jugador varchar(50),
+No_Movimiento int,
 IdJugador varchar(60) not null,
 IdPartida int not null,
 CONSTRAINT fk_Player FOREIGN KEY (IdJugador) REFERENCES Usuario (Nom_Usuario),
@@ -30,8 +31,16 @@ CONSTRAINT fk_Part FOREIGN KEY (IdPartida) REFERENCES Partida(Id_Partida)
 
 CREATE TABLE Torneo(
 Id_Torneo int primary key identity NOT NULL,
-Estado_Jugador varchar(60) not null,
-No_Torneos int not null,
-IdJugador varchar(60) not null,
-CONSTRAINT fk_Jugador FOREIGN KEY (IdJugador) REFERENCES Usuario (Nom_Usuario)
+No_Jugadores int,
+Fecha date
 );
+
+CREATE TABLE Dato_Torneo(
+DatoTorneo int primary key identity NOT NULL,
+Estado_Jugador varchar(50),
+IdJugador varchar(60) not null,
+IdTorneo int not null,
+CONSTRAINT fk_jugador FOREIGN KEY (IdJugador) REFERENCES Usuario (Nom_Usuario),
+CONSTRAINT fk_Toreno FOREIGN KEY (IdTorneo) REFERENCES Torneo (Id_Torneo),
+);
+
