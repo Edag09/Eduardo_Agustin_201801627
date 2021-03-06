fba1b451068d4f748668af2b837b7ce745571f98
Eduaro Agustin <3006840880101@ingenieria.usac.edu.gt>
2021-03-06 01:29:35 -0600

    Suma

--- IPC2_1S2021/Proyecto1/LS.py
+++ IPC2_1S2021/Proyecto1/LS.py
@@ -5,6 +5,7 @@ class Simple_list:
     def __init__(self):
         self.root = None
         self.tam = 0
+        self.n =0
 
     def addSimple(self, x, y, value, n):
         first = nodeSimple(x, y, value)
@@ -22,6 +23,7 @@ class Simple_list:
         while aux is not None:
             print(' Posicion X: ', str(aux.x) + ' Posicion Y: ', str(aux.y) + ' Valor: ', str(aux.value))
             aux = aux.next
+        print('\n')
 
     def binaria(self):
         aux = self.root
@@ -34,4 +36,26 @@ class Simple_list:
             if aux.y == self.tam:
                 temp = temp + '\n'
             aux = aux.next
-        print(temp)
+        Simple_list.comparar(self, temp, self.tam)
+
+    def comparar(self, text, row):
+        aux = self.root
+        temp = ''
+        while aux is not None:
+            if aux.y <= row:
+                temp = temp + str(aux.value)
+                if aux.y == row:
+                    temp = temp + '\n'
+            aux = aux.next
+        b = temp.split('\n')
+        a = text.split('\n')
+        cont = 0
+        while cont <= (int(row)-1):
+            print(b[cont] + ' ---> ' + a[cont])
+            cont += 1
+
+
+
+
+
+
